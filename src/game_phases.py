745e6bc25a55e4b4748c95936f63f62019079907
Am1r__8 <68626539+CoachAlpha@users.noreply.github.com>
2023-02-20 17:23:50 -0500

    phases

--- src/game_phases.py
+++ src/game_phases.py
@@ -48,3 +48,29 @@ def main_menu_phase():
     VisualizationService.draw_background_with_scroll(GlobalState.SCREEN, GlobalState.SCROLL)
     GlobalState.PRESS_Y = update_press_key(GlobalState.PRESS_Y)
     VisualizationService.draw_main_menu(GlobalState.SCREEN, scoreboard.get_max_score(), GlobalState.PRESS_Y)
+
+def gameplay_phase():
+    events = pygame.event.get()
+
+    for event in events:
+        if is_close_app_event(event):
+            game_over()
+            return
+
+    P1.update()
+    H1.move(scoreboard, P1.player_position)
+    H2.move(scoreboard, P1.player_position)
+
+    GlobalState.SCROLL = update_background_using_scroll(GlobalState.SCROLL)
+    VisualizationService.draw_background_with_scroll(GlobalState.SCREEN, GlobalState.SCROLL)
+
+    P1.draw(GlobalState.SCREEN)
+    H1.draw(GlobalState.SCREEN)
+    H2.draw(GlobalState.SCREEN)
+    scoreboard.draw(GlobalState.SCREEN)
+
+    if pygame.sprite.spritecollide(P1, hands, False, pygame.sprite.collide_mask):
+        scoreboard.update_max_score()
+        MusicService.play_slap_sound()
+        time.sleep(0.5)
+        game_over()
