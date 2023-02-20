f1eb153b825b0117056cee12b49466ab9233f0f4
Am1r__8 <68626539+CoachAlpha@users.noreply.github.com>
2023-02-20 17:21:05 -0500

    main func

--- main.py
+++ main.py
@@ -14,3 +14,15 @@ FramePerSec = pygame.time.Clock()
 def update_game_display():
     pygame.display.update()
     FramePerSec.tick(Config.FPS)
+
+def main():
+    while True:
+        if GlobalState.GAME_STATE == GameStatus.MAIN_MENU:
+            main_menu_phase()
+        elif GlobalState.GAME_STATE == GameStatus.GAMEPLAY:
+            gameplay_phase()
+        elif GlobalState.GAME_STATE == GameStatus.GAME_END:
+            exit_game_phase()
+
+        MusicService.start_background_music()
+        update_game_display()
