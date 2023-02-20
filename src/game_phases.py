de4c55f9e32d85b4412e843096e52607d81ce447
Am1r__8 <68626539+CoachAlpha@users.noreply.github.com>
2023-02-20 17:23:39 -0500

    phases

--- src/game_phases.py
+++ src/game_phases.py
@@ -30,3 +30,21 @@ all_sprites = pygame.sprite.Group()
 all_sprites.add(P1)
 all_sprites.add(H1)
 all_sprites.add(H2)
+
+def main_menu_phase():
+    scoreboard.reset_current_score()
+
+    events = pygame.event.get()
+
+    for event in events:
+        if is_close_app_event(event):
+            GlobalState.GAME_STATE = GameStatus.GAME_END
+            return
+
+        if event.type == pygame.KEYDOWN:
+            GlobalState.GAME_STATE = GameStatus.GAMEPLAY
+
+    GlobalState.SCROLL = update_background_using_scroll(GlobalState.SCROLL)
+    VisualizationService.draw_background_with_scroll(GlobalState.SCREEN, GlobalState.SCROLL)
+    GlobalState.PRESS_Y = update_press_key(GlobalState.PRESS_Y)
+    VisualizationService.draw_main_menu(GlobalState.SCREEN, scoreboard.get_max_score(), GlobalState.PRESS_Y)
