2c15e63f8829153d64b40e78e9e007572407c721
Am1r__8 <68626539+CoachAlpha@users.noreply.github.com>
2023-02-20 17:23:30 -0500

    phases

--- src/game_phases.py
+++ src/game_phases.py
@@ -17,3 +17,16 @@ GlobalState.load_main_screen()
 VisualizationService.load_main_game_displays()
 
 scoreboard = Scoreboard()
+
+P1 = Player()
+H1 = Hand(HandSide.RIGHT)
+H2 = Hand(HandSide.LEFT)
+
+# Sprite Groups
+hands = pygame.sprite.Group()
+hands.add(H1)
+hands.add(H2)
+all_sprites = pygame.sprite.Group()
+all_sprites.add(P1)
+all_sprites.add(H1)
+all_sprites.add(H2)
