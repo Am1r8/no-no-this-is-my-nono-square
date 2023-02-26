6341a2518c048b761c339f75d09bf91cc2dd0880
Am1r__8 <68626539+CoachAlpha@users.noreply.github.com>
2023-02-25 20:37:31 -0500

    now visualize

--- src/services/visualization_service.py
+++ src/services/visualization_service.py
@@ -3,3 +3,12 @@ import pygame
 from paths import ASSETS_DIR, MENU_DIR
 from src.config import Config
 from src.utils.tools import sine
+
+class VisualizationService:
+    @staticmethod
+    def get_right_hand_image():
+        return pygame.image.load(ASSETS_DIR / "right_hand.png").convert_alpha()
+
+    @staticmethod
+    def get_left_hand_image():
+        return pygame.image.load(ASSETS_DIR / "left_hand.png").convert_alpha()
