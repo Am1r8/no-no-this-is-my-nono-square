9124bb7f185210fc4458ab0a602fd7a488b8f374
Am1r__8 <68626539+CoachAlpha@users.noreply.github.com>
2023-02-25 20:35:02 -0500

    service

--- src/services/music_service.py
+++ src/services/music_service.py
@@ -3,3 +3,12 @@ import random
 import pygame
 
 from paths import AUDIO_DIR
+
+class MusicService:
+    @staticmethod
+    def get_background_musics():
+        return [
+            AUDIO_DIR / "sleigh_ride.ogg",
+            AUDIO_DIR / "merry_christmas.ogg",
+            AUDIO_DIR / "here_comes_santa.ogg"
+        ]
