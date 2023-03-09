a2b29fb0197b7b5b2945e4c5e9df2febc17357e5
Am1r__8 <68626539+CoachAlpha@users.noreply.github.com>
2023-03-08 20:35:05 -0500

    hand

---
+++ src/components/hand.py
@@ -0,0 +1,10 @@
+import random
+
+import pygame
+
+from src.components.hand_side import HandSide
+from src.components.scoreboard import Scoreboard
+from src.config import Config
+from src.services.music_service import MusicService
+from src.services.visualization_service import VisualizationService
+from src.utils.tools import sine
